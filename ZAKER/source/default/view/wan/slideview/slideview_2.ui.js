f5b96402a33c14e5b45bb729a18f6b18f080e160
liujian <6j1008@163.com>
2015-09-28 20:43:34 +0800

    9.28

--- ZAKER/source/default/view/wan/slideview/slideview_2.ui.js
+++ ZAKER/source/default/view/wan/slideview/slideview_2.ui.js
@@ -1,8 +1,10 @@
 /**********************************************************************************************************/
 var app = sm("do_App");
+var page = sm("do_Page");
 var storage = sm("do_Storage");
 var listview = ui("do_listview_1");
 var listdata = mm("do_ListData");
+var global = sm("do_Global");
 
 listview.bindItems(listdata);// 建立ListView 与 ListData 的行数据关系;
 
@@ -13,5 +15,7 @@ storage.readFile("data://wan_2.json", function(data){// 读取文件
 
 var nf = sm("do_Notification");
 listview.on("touch", function(index) {
-    nf.alert(listdata.getOne(index), "touch");
+//	page.fire("wan-which", index);
+	global.setMemory("key","1");
+	app.openPage({source:"source://view/wan/wan_de.ui", data:index, animationType:"", isFullScreen:false, keyboardMode:"default", scriptType:""}, function(data, e){});
 });
